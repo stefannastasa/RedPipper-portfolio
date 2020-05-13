4432f5a02696b980fae38db5dc0f2b8cf457e8eb
Stefan Nastasa <stefan21nastasa@gmail.com>
2020-05-13 18:20:20 +0300

    Animation changes

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -12,8 +12,39 @@ function loader(){
     type.start();
 }
 
+function toggleAnimation(){
+    profile = document.getElementsByClassName("data-profile")[0];
+    button = document.getElementsByClassName("ic-btn")[0];
+    github = document.getElementsByClassName("data-github")[0];
+    
+    if(profile.classList.contains("hide-profile") || profile.classList.contains("show-profile")){
+        profile.classList.toggle("hide-profile");
+        profile.classList.toggle("show-profile");
+    }
+    else profile.classList.add("hide-profile");
+    
+    if(github.classList.contains("hide-github") || github.classList.contains("show-github")){
+        github.classList.toggle("hide-github");
+        github.classList.toggle("show-github");
+    }
+    else github.classList.add("show-github");
+
+    profile.hidden = !profile.hidden;
+    github.hidden = !github.hidden;
+    
+
+    
+}
+
 $(document).ready(function(){
     loader();
-    
+    setTimeout(()=>{
+        profile = document.getElementsByClassName("data-profile")[0];
+        if(profile.classList.contains("first-entry"))
+            profile.classList.remove("first-entry");
+    },5500);
 })
-document.getElementsByClassName("ic-btn").onclick = function(){toggleAnimation()}
+
+button = document.getElementsByClassName("ic-btn"); 
+button[0].addEventListener("click", toggleAnimation);
+//button[1].addEventListener("click", toggleAnimation);
